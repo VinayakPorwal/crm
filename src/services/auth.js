613a7a9fcd186ea2161174f8d9425020c716fb57
fookrey420 <84093231+fookrey420@users.noreply.github.com>
2023-11-15 02:59:03 +0530

    client Crm

--- src/services/auth.js
+++ src/services/auth.js
@@ -5,15 +5,32 @@ export default {
     return api.post("/auth/signup", credentials);
   },
   verifyOTP: function (credentials) {
-    return api.post("/auth/verify", credentials);
+    return api.post(
+      "/auth/verify",
+      { body: JSON.stringify(credentials) },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   },
   login: function (credentials) {
-    return api.post("/auth/login", credentials);
+    return api.post(
+      "/auth/login",
+      { body: JSON.stringify(credentials) },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   },
   verifyUser: function (token) {
     return api.post("/auth/getUser", token, {
       headers: {
         authorization: token,
+        "Content-Type": "application/json",
         // other headers if needed
       },
     });
