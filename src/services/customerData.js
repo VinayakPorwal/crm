5b3a84e0d004dafa394c56972b1de9fedd5004be
fookrey420 <84093231+fookrey420@users.noreply.github.com>
2023-11-15 11:05:28 +0530

    client Crm

--- src/services/customerData.js
+++ src/services/customerData.js
@@ -5,18 +5,26 @@ export default {
     return api.get("/customer/list", {
       headers: {
         authorization: token,
+        "Access-Control-Allow-Origin": "*",
+
         // other headers if needed
       },
     });
   },
   interactions: function (credentials) {
-    return api.post("/customer/interactions", credentials);
+    return api.post("/customer/interactions", credentials, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
   },
 
   ViewProfile: function (id, token) {
     return api.get("/customer/profile/" + id, {
       headers: {
         authorization: token,
+        "Access-Control-Allow-Origin": "*",
+
         // other headers if needed
       },
     });
